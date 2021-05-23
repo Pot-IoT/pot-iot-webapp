3f11822a75f171be7cac4b7f0ad81ae836236e48
Yuan <yuan.li@pot-iot.com>
2021-05-23 13:54:44 +0200

    Update Documentation page

--- src/components/HomePageRepo/Documentation/DocData.js
+++ src/components/HomePageRepo/Documentation/DocData.js
@@ -1,21 +1,49 @@
-export const docAdd = {
-    titleSection: 'Add Device',
+export const docAddAccount = {
+    titleSection: 'Step 1 : Create a POT-IOT account',
     stepDescription:
-        ' 1) By clicking Add Device button, bring out the device addition dialog. \n' +
-        '\n' +
-        ' 2) Complete the form: Device Name (MANDATORY), Device Description, then click Add button to submit. \n' +
-        '\n' +
-        ' 3) A successful window will be shown right after the device addition dialog, save and well protect the information because for the safty reason, the Private Key will not be shown after the window is closed'
+        '* Visit https://www.pot-iot.com/signup and sign up for a new account.'
+}
+
+export const docAdd = {
+    titleSection: 'Step 2 : Create a Device',
+    stepDescription:
+        '* 2.1 - Login to your POT-IOT account.\n\n'
+        +
+        '* 2.2 - Click "NEW DEVICE" button.\n\n'
+        +
+        '* 2.3 - Enter the Device Name, Description of the device, then click "ADD" button\n\n'
+        +
+        '* 2.4 - A successful window will be shown. Device ID and Private Key are generated for the device. Please click "SAVE TO LOCAL" button to download these information.\n'
 }
 
 export const docConnect = {
-    titleSection: 'Connet device to platform',
+    titleSection: 'Step 3 : Connect IOT device to POT-IOT and upload data files',
     stepDescription:
-        ' 1) \n' +
+        '* 3.1 - Visit our GitHub repo https://github.com/Pot-IoT/potiot-example-raspberry-pi and download the example code to Raspberry Pi.\n' +
         '\n' +
-        ' 2) \n' +
+        '* 3.2 - Follow the instruction on the repo page: Change device_id, private_key using the information collected from step 2.4 and set the file type and path of the file to be uploaded to POT-IOT.  \n' +
         '\n' +
-        ' 3) '
+        '* 3.3 - Run "example.py" on the Raspberry PI and make sure it has access to the Internet. \n' +
+        '\n' +
+        '* 3.4 - If return code is 200, it means your device is successfully connected to POT-IOT platform and the file is uploaded. \n' +
+        '\n' +
+        '* 3.5 - If return code is other than 200, an error occured. Please check if your device ID, private key are correct and your device has access to the Internet.'
+}
+
+export const docDownload = {
+    titleSection: 'Step 4 : Download data files on POT-IOT',
+    stepDescription:
+        '* 4.1 - Go back to POT-IOT device list page and click on the device which uploaded data file as the step 3. \n' +
+        '\n' +
+        '* 4.2 - On the detail page, you can find all the files uploaded. \n' +
+        '\n' +
+        '* 4.3 - Click "Download" button of the selected file to start download.'
+}
+
+export const docGitHub = {
+    titleSection: 'GitHub Repo',
+    stepDescription:
+        '* https://github.com/Pot-IoT/potiot-example-raspberry-pi\n'
 }
 
 export const docEdit = {
@@ -38,12 +66,3 @@ export const docCommand = {
         ' 3) Scorll down to the second section, sending custom command divided by "," in String like.'
 }
 
-export const docDownload = {
-    titleSection: 'Download data package',
-    stepDescription:
-        ' 1) Choose a device that you want to manipulate with in Device List.\n' +
-        '\n' +
-        ' 2) Hover on that device and click to get into the deivce detail page.\n' +
-        '\n' +
-        ' 3) Scorll down to the third section, click the link to download target data package.'
-}
--- src/components/HomePageRepo/Documentation/Documentation.js
+++ src/components/HomePageRepo/Documentation/Documentation.js
@@ -3,11 +3,13 @@ import { Divider } from "../Divider/Divider";
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 import {
+  docAddAccount,
   docAdd,
   docConnect,
   docEdit,
   docCommand,
   docDownload,
+  docGitHub,
 } from "./DocData";
 import DocSection from "./DocSection";
 import "./Documentation.css";
@@ -18,27 +20,36 @@ function Documentation() {
         <Navbar />
         <div className="doc-page-root">
         <div className="doc-top-section" id="doc-top-section">
-            <h3 class="heading color-black">PoI-Iot Platform Documentation</h3>
+            <h3 class="heading color-black">Getting Start With POT-IOT</h3>
             <div class="text-top color-black">
             {" "}
-            With this instruction, you will be guided by sections, steps, and
-            findally connect to the Platform and enjoy how easy it is. If there is
-            something hard to understand, do not hesitate to contact us via Email:
+            In this section we will walk through registering a IOT device on POT-IOT 
+            platform and uploading sensor data to it using an IOT device (a Raspberry 
+            Pi in this tutorial as example).<br></br> Having trouble connecting your 
+            device ? Do not hesitate to contact us via Email:
             support@pot-iot.com.
             </div>
         </div>
         <Divider
-            text="PoT-IoT"
+            text="PoT-IoT Quick Start Guide"
             margin="30px"
             color="#276afb"
             lineColor="#276afb"
         />
         <div className="doc-body">
+            <DocSection {...docAddAccount} />
             <DocSection {...docAdd} />
             <DocSection {...docConnect} />
-            <DocSection {...docEdit} />
-            <DocSection {...docCommand} />
             <DocSection {...docDownload} />
+        </div>
+        <Divider
+            text="Find the full example on GitHub"
+            margin="30px"
+            color="#276afb"
+            lineColor="#276afb"
+        />
+        <div className="doc-body">
+            <DocSection {...docGitHub} />
         </div>
         </div>
         <Footer />
